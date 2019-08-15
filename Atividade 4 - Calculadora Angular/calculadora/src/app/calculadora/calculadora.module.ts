809a4b76176979ae161ee0d5ad6f0feefaa1cc64
thalison <thalison.jp@gmail.com>
2019-08-15 16:49:18 -0300

    Add files via upload

---
+++ Atividade 4 - Calculadora Angular/calculadora/src/app/calculadora/calculadora.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { CalculadoraService } from './services/calculadora.service';
+
+
+
+@NgModule({
+  declarations: [CalculadoraComponent],
+  imports: [
+    CommonModule
+  ],
+  exports : [
+    CalculadoraComponent
+  ],
+  providers: [
+    CalculadoraService
+  ]
+})
+export class CalculadoraModule { }
